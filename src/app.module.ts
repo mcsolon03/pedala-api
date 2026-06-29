d1486f8485419030669b87a8f10a95dee6fbde8a
Clara <solozinhauiui69@gmail.com>
2026-06-29 10:23:46 -0300

    criando modulo de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsuarioModule],
   providers: [AppService],
 })
 export class AppModule {}
